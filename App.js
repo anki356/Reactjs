ea11e99a59d6f2a75ed85b59b071f9142ff315e3
anki356 <39798400+anki356@users.noreply.github.com>
2021-08-18 18:20:40 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import Head from './Components/head';
+import Searchbox from './Components/searchbox';
+import Wrapper from './Components/wrapper'
+
+import './App.css'
+class App extends Component {
+    render(){
+        return(
+          
+        <div className="App"> 
+        
+       
+         <Head></Head>
+        <Searchbox></Searchbox>
+        <Wrapper></Wrapper>
+      </div> 
+       
+      )}} 
+         
+       
+         
+          
+
+       
+export default App;
+
+
